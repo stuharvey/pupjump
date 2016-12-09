cb5ac6fa73c37d3c4e53790204f4f2e37f8c66ab
stuharvey <stu@vt.edu>
2016-12-09 17:24:04 -0500

    Refactor image code slightly; Update movement constants

--- game/Platform.js
+++ game/Platform.js
@@ -8,9 +8,8 @@ export default class Platform extends Rectangle {
     super(args.pos.x, args.pos.y, PLAT.WIDTH, PLAT.HEIGHT);
     this.type = args.type;
     this.falling = false;
+    this.images = args.images;
   }
-
-  // So we can get the platform width anywhere
 
   update (state, delta) {
     if (this.falling) {
@@ -34,3 +33,39 @@ export default class Platform extends Rectangle {
     ctx.restore();
   }
 }
+
+// Alternate drawing style... broken for some reason
+// let delta  = 2;
+// // background coordinates
+// let rLeft   = this.left - delta;
+// let rRight  = this.right - delta;
+// let rTop    = this.top - delta;
+// let rBot    = this.bottom - delta;
+// // foreground coordinates
+// let fLeft  = this.left + delta;
+// let fRight = this.right + delta;
+// let fBot   = this.bottom + delta;
+// let fTop   = this.top + delta;
+//
+// let rLeftTop   = [rLeft,  rTop];
+// let rRightTop  = [rRight, rTop];
+// let rLeftBot   = [rLeft,  rBot];
+// let fLeftTop  = [fLeft, fTop];
+// let fLeftBot  = [fLeft, fBot];
+// let fRightTop = [fRight, fTop];
+// let fRightBot = [fRight, fBot];
+//
+// // front face
+// let frontFace = [fLeftTop, fRightTop, fRightBot, fLeftBot];
+// // top face
+// let topFace = [rLeftTop, rRightTop, fRightTop, fLeftTop];
+// // side face
+// let sideFace = [rLeftTop, fLeftTop, fLeftBot, rLeftBot];
+// let faces = [frontFace, sideFace, topFace];
+// for (let i = 0; i < faces.length; i++) {
+//   let face = faces[i];
+//   ctx.moveTo(face[0][0], face[0][1]);
+//   [face[1], face[2], face[3], face[0]].forEach(p => ctx.lineTo(p[0], p[1]));
+//   ctx.stroke();
+//
+// }
