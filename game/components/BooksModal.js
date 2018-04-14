e20a22774b6a475072c3048c82e2ea7b5ec83ff2
stuharvey <stu@vt.edu>
2018-04-14 10:31:36 -0400

    Implement booklist scaffold/prototype

--- game/components/BooksModal.js
+++ game/components/BooksModal.js
@@ -1,30 +1,63 @@
 import React, { Component } from 'react';
 
 import Modal from 'react-modal';
+import BOOKS from '../data/BookList';
 
+const Book = ({
+  title,
+  link,
+  description,
+  stars,
+  status,
+  year,
+}) => (
+    <div style={{ margin: 10, padding: 10, backgroundColor: '#fafafa' }}>
+      <h3>
+        {link ? (
+          <a style={{ color: 'black' }} href={link}>
+            {title}
+          </a>
+        ) : (
+            title
+          )}
+        <span style={{ float: 'right', color: 'darkgrey', fontWeight: 300, }}>
+          {stars} stars
+        </span>
+      </h3>
+      <p>
+        {description}
+      </p>
+    </div>
+  )
 
-export default class BooksModal extends Component {
-  render() {
-    return (
-      <Modal
-        isOpen={this.props.active}
-        contentLabel="booksModal"
-        style={{
-          content: {
-            'z-index': 2,
-            top: '80px',
-          }
-        }}>
-        <h2>
-          Books I've read:
-        </h2>
-        <h3>
-          2017
-        </h3>
-        <h3>
-          2016
-        </h3>
-      </Modal>
-    );
-  }
-}
+const BooksModal = ({ active }) => (
+  <Modal
+    isOpen={active}
+    style={{
+      content: {
+        padding: 20,
+        top: 90,
+      }
+    }}
+    contentLabel="booksModal"
+  >
+    <h2>
+      Books I've read
+    </h2>
+    {Object.entries(BOOKS
+      .reduce((booksInYear, book) => (
+        { ...booksInYear, [book.yearRead]: [...booksInYear[book.yearRead], book] }
+      ), {2018: [], 2017: []}))
+      .map(([year, books]) =>
+        <div>
+          <h2>
+            {year}
+          </h2>
+          {books.map(book => <Book {...book} />)}
+        </div>
+      )
+    }
+  </Modal>
+)
+
+export default BooksModal
